9bb5c66f946fad29415e42fa59b9e9e9404ae8a5
Solakhuddin <80704147+Solakhuddin@users.noreply.github.com>
2024-01-23 10:34:28 +0700

    Update  checkbox  file hapus_npwp.php
    memperbaiki bug checkbox

--- profil/hapus_npwp.php
+++ profil/hapus_npwp.php
@@ -73,52 +73,52 @@ $row	= mysqli_fetch_assoc($profil);
                 <hr>
                 <h4 class="font-weight-bold">ALASAN PENGHAPUSAN NPWP</h4>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak orang pribadi telah meninggal dunia dan tidak meninggalkan warisan</label>
+                    <input type="checkbox" class="form-check-input" id="cek1">
+                    <label class="form-check-label" for="cek1">Wajib Pajak orang pribadi telah meninggal dunia dan tidak meninggalkan warisan</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak bendahara pemerintah yang tidak lagi memenuhi syarat sebagai Wajib Pajak karena yang bersangkutan sudah tidak lagi melakukan pembayaran</label>
+                    <input type="checkbox" class="form-check-input" id="cek2">
+                    <label class="form-check-label" for="cek2">Wajib Pajak bendahara pemerintah yang tidak lagi memenuhi syarat sebagai Wajib Pajak karena yang bersangkutan sudah tidak lagi melakukan pembayaran</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak orang pribbadi yang telah meninggalkan indonesia untuk selama-lamanya</label>
+                    <input type="checkbox" class="form-check-input" id="cek3">
+                    <label class="form-check-label" for="cek3">Wajib Pajak orang pribbadi yang telah meninggalkan indonesia untuk selama-lamanya</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib pajak yang memiliki lebih dari 1(satu) Nomor Pokok Wajib Pajak untuk menentukan Nomor Pokok Wajib Pajak yang dapat digunakan sebagai sarana administratif dalam pelaksanaan hak dan pemenuhan kewajiban perpajakan</label>
+                    <input type="checkbox" class="form-check-input" id="cek4">
+                    <label class="form-check-label" for="cek4">Wajib pajak yang memiliki lebih dari 1(satu) Nomor Pokok Wajib Pajak untuk menentukan Nomor Pokok Wajib Pajak yang dapat digunakan sebagai sarana administratif dalam pelaksanaan hak dan pemenuhan kewajiban perpajakan</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak orang pribadi yang berstatus sebagai pengurus, komisaris, pemegang saham,/pemilik dan pegawai yang telah diberikan Nomor Pokok Wajib Pajak melalui pemberian kerja/bendahara pemerintahan dan pennghasilan netonya tidak melibihi Penghasilan Tidak Kena Pajak</label>
+                    <input type="checkbox" class="form-check-input" id="cek5">
+                    <label class="form-check-label" for="cek5">Wajib Pajak orang pribadi yang berstatus sebagai pengurus, komisaris, pemegang saham,/pemilik dan pegawai yang telah diberikan Nomor Pokok Wajib Pajak melalui pemberian kerja/bendahara pemerintahan dan pennghasilan netonya tidak melibihi Penghasilan Tidak Kena Pajak</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak badan kantor perwakilan perusahaan asing yang tidak mempunyai kewajiban Pajak Penghasil badan yang telah menghentikan kegiatan usahanya</label>
+                    <input type="checkbox" class="form-check-input" id="cek6">
+                    <label class="form-check-label" for="cek6">Wajib Pajak badan kantor perwakilan perusahaan asing yang tidak mempunyai kewajiban Pajak Penghasil badan yang telah menghentikan kegiatan usahanya</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Warisan yang belum terbagi dalam kedudukan sebagai Subjek Pajak sudah selesai dibagi</label>
+                    <input type="checkbox" class="form-check-input" id="cek7">
+                    <label class="form-check-label" for="cek7">Warisan yang belum terbagi dalam kedudukan sebagai Subjek Pajak sudah selesai dibagi</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wanita yang sebelumnya telah memiliki Nomor Pokok Wajib Pajak dan menikah tanpa membuat perjanjian pemisahan harta dan penghasilan serta tidak ingin melaksanakan hak dan memenuhi kewajiban perpajakannya terpisah dari suaminya</label>
+                    <input type="checkbox" class="form-check-input" id="cek8">
+                    <label class="form-check-label" for="cek8">Wanita yang sebelumnya telah memiliki Nomor Pokok Wajib Pajak dan menikah tanpa membuat perjanjian pemisahan harta dan penghasilan serta tidak ingin melaksanakan hak dan memenuhi kewajiban perpajakannya terpisah dari suaminya</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wanita Kawin yang memiliki memiliki Nomor Pokok Wajib Pajak berbeda dengan Nomor Pokok Wajib Pajak suami dan pelaksanaan hak dan pemenuhan kewajiban perpajakannya digabungkan dengan pelaksanaan hak dan pemenuhan kewajiban perpajakan suami</label>
+                    <input type="checkbox" class="form-check-input" id="cek9">
+                    <label class="form-check-label" for="cek9">Wanita Kawin yang memiliki memiliki Nomor Pokok Wajib Pajak berbeda dengan Nomor Pokok Wajib Pajak suami dan pelaksanaan hak dan pemenuhan kewajiban perpajakannya digabungkan dengan pelaksanaan hak dan pemenuhan kewajiban perpajakan suami</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Anak belum dewasa yang telah memiliki Nomor Pokok Wajib Pajak</label>
+                    <input type="checkbox" class="form-check-input" id="cek10">
+                    <label class="form-check-label" for="cek10">Anak belum dewasa yang telah memiliki Nomor Pokok Wajib Pajak</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak bentuk usaha tetap yang telah menghentikan kegiatan usahanya di indonesia</label>
+                    <input type="checkbox" class="form-check-input" id="cek11">
+                    <label class="form-check-label" for="cek11">Wajib Pajak bentuk usaha tetap yang telah menghentikan kegiatan usahanya di indonesia</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Wajib Pajak badan tertentu selain perseroan terbatas dengan status tidak aktif (non efektif) yang tidak mempunyai kewajiban Pajak Penghasilan dan secara nyata tidak menunjukkan adanya kegiatan usaha</label>
+                    <input type="checkbox" class="form-check-input" id="cek12">
+                    <label class="form-check-label" for="cek12">Wajib Pajak badan tertentu selain perseroan terbatas dengan status tidak aktif (non efektif) yang tidak mempunyai kewajiban Pajak Penghasilan dan secara nyata tidak menunjukkan adanya kegiatan usaha</label>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Alasan Lain :</label>
@@ -128,8 +128,8 @@ $row	= mysqli_fetch_assoc($profil);
                 <hr>
                 <h4 class="font-weight-bold">PERNYATAAN</h4>
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Dengan menyadari sepenuhnya akan segala akibatnya termasuk sanksi-sanksi sesuai dengan ketentuan perundang-undangan yang berlaku, saya menyatakan bahwa apa yang telah saya beritahukan di atas adalah benar dan lengkap</label>
+                    <input type="checkbox" class="form-check-input" id="cek14">
+                    <label class="form-check-label" for="cek14">Dengan menyadari sepenuhnya akan segala akibatnya termasuk sanksi-sanksi sesuai dengan ketentuan perundang-undangan yang berlaku, saya menyatakan bahwa apa yang telah saya beritahukan di atas adalah benar dan lengkap</label>
                 </div>
                 <br>
                 <button type="submit" class="btn btn-primary">Submit</button>
