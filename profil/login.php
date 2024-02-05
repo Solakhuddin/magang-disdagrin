693b4590b573f22bc175a585fcb9bef86a7fa334
Solakhuddin <80704147+Solakhuddin@users.noreply.github.com>
2024-02-05 21:05:27 +0700

    Update login.php

--- profil/login.php
+++ profil/login.php
@@ -99,12 +99,9 @@ if(isset($_POST['submit'])){
               </script>';
         @session_destroy();
     } else {        
-        // Establish database connection
-        $mysqli = new mysqli("localhost", "username", "password", "database");
-
         // Check if the connection is successful
-        if($mysqli->connect_error) {
-            die("Connection failed: " . $mysqli->connect_error);
+        if($koneksi->connect_error) {
+            die("Connection failed: " . $koneksi->connect_error);
         }
 
         // Variables for username and password
@@ -114,7 +111,7 @@ if(isset($_POST['submit'])){
         $expired = 300000;
 
         // Prepare the SQL statement
-        $stmt = $mysqli->prepare("SELECT * FROM userlogin WHERE replace(UserName,' ','') = replace(?,' ','') AND UserPsw = ? AND IsAktif = b'1'");
+        $stmt = $koneksi->prepare("SELECT * FROM userlogin WHERE replace(UserName,' ','') = replace(?,' ','') AND UserPsw = ? AND IsAktif = b'1'");
         $stmt->bind_param("ss", $username, $password);
 
         // Execute the prepared statement
@@ -161,7 +158,7 @@ if(isset($_POST['submit'])){
         $stmt->close();
         
         // Close the database connection
-        $mysqli->close();
+        $koneksi->close();
     }
 }
 // script lama 
