871078990f9d9a1964d509e9f05b6dbd4eb48b12
Solakhuddin <80704147+Solakhuddin@users.noreply.github.com>
2024-02-11 16:00:37 +0700

    Update TrTeraStandart_aksi.php

--- admin/TrTeraStandart_aksi.php
+++ admin/TrTeraStandart_aksi.php
@@ -19,20 +19,51 @@ if(isset($_POST['SimpanData'])){
 	$Tanggal = date('Ymd');
 	$TanggalNOW = date("Y-m-d");
 
-	$sql = @mysqli_query($koneksi, "SELECT RIGHT(NoTransaksi,8) AS kode FROM tractiontimbangan ORDER BY NoTransaksi DESC LIMIT 1"); 
-	$nums = mysqli_num_rows($sql);
-	if($nums <> 0){
-		 $data = mysqli_fetch_array($sql);
-		 $kode = $data['kode'] + 1;
-	}else{
-		 $kode = 1;
+
+	$query = "SELECT RIGHT(NoTransaksi, 8) AS kode FROM tractiontimbangan ORDER BY NoTransaksi DESC LIMIT 1";
+	$stmt = mysqli_prepare($koneksi, $query);
+
+	mysqli_stmt_execute($stmt);
+
+	mysqli_stmt_bind_result($stmt, $kode);
+
+	mysqli_stmt_fetch($stmt);
+
+	if (mysqli_stmt_num_rows($stmt) !== 0) {
+		$kode += 1;
+	} else {
+		$kode = 1;
 	}
+
+	mysqli_stmt_close($stmt);
+
+	// $sql = @mysqli_query($koneksi, "SELECT RIGHT(NoTransaksi,8) AS kode FROM tractiontimbangan ORDER BY NoTransaksi DESC LIMIT 1"); 
+	// $nums = mysqli_num_rows($sql);
+	// if($nums <> 0){
+	// 	 $data = mysqli_fetch_array($sql);
+	// 	 $kode = $data['kode'] + 1;
+	// }else{
+	// 	 $kode = 1;
+	// }
+
 	//mulai bikin kode
 	 $bikin_kode = str_pad($kode, 8, "0", STR_PAD_LEFT);
 	 $kode_jadi = "TR-".$Tanggal."-".$bikin_kode;
 
-	 $SimpanData = @mysqli_query($koneksi, "INSERT INTO tractiontimbangan (NoTransaksi,IDPerson,JenisTransaksi,UserTerima,UserTera,UserAmbil,TglTransaksi,StatusTransaksi,TotalRetribusi,IsDibayar,IsDiambil,TglAmbil,TglTera)VALUES('$kode_jadi','$IDPerson','TERA DI LOKASI','$UserName','$UserName','$UserName', DATE(NOW()), 'SELESAI','0',b'1',b'1',NOW(),NOW())"); 
-
+	 
+	 // Prepare the SQL statement for inserting the main transaction
+	 $insertQuery = "INSERT INTO tractiontimbangan (NoTransaksi,IDPerson,JenisTransaksi,UserTerima,UserTera,UserAmbil,TglTransaksi,StatusTransaksi,TotalRetribusi,IsDibayar,IsDiambil,TglAmbil,TglTera) VALUES (?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW())";
+	 
+	 // Prepare the statement
+	 $insertStmt = mysqli_prepare($koneksi, $insertQuery);
+	 
+	 // Bind parameters
+	 mysqli_stmt_bind_param($insertStmt, "sissssssiiss", $kode_jadi, $IDPerson, $JenisTransaksi, $UserName, $UserName, $UserName, $TglTransaksi, $StatusTransaksi, $TotalRetribusi, $IsDibayar, $IsDiambil);
+	 
+	 $SimpanData = mysqli_stmt_execute($insertStmt);
+	 
+	 //  $SimpanData = @mysqli_query($koneksi, "INSERT INTO tractiontimbangan (NoTransaksi,IDPerson,JenisTransaksi,UserTerima,UserTera,UserAmbil,TglTransaksi,StatusTransaksi,TotalRetribusi,IsDibayar,IsDiambil,TglAmbil,TglTera)VALUES('$kode_jadi','$IDPerson','TERA DI LOKASI','$UserName','$UserName','$UserName', DATE(NOW()), 'SELESAI','0',b'1',b'1',NOW(),NOW())"); 
+	 
 	if($SimpanData){
 
 		 for ($i=0; $i < sizeof($_POST['IDTimbangan']); $i++) {
@@ -43,16 +74,36 @@ if(isset($_POST['SimpanData'])){
 			$NoUrutTrans	=($i+1);
 
 			@$Status = isset($HasilAction1) && $HasilAction1 === "TERA BATAL" ? "Non Aktif" : "Aktif";
-			mysqli_query($koneksi,"update timbanganperson set StatusUTTP='$Status' where IDTimbangan='$IDTimbangan'");
 			
-            $DataItem = @mysqli_query($koneksi, "INSERT INTO trtimbanganitem (NoTransaksi,NoUrutTrans,IDPerson,IDTimbangan,UserName,NominalRetribusi,HasilAction1,HasilAction2,TanggalTransaksi)VALUES('$kode_jadi','$NoUrutTrans','$IDPerson','$IDTimbangan','$UserName','0','$HasilAction1','$HasilAction2','$TanggalNOW')"); 
-        }
+			// Update timbanganperson table
+			$updateTimbanganPersonQuery = "UPDATE timbanganperson SET StatusUTTP = ? WHERE IDTimbangan = ?";
+			$updateTimbanganPersonStmt = mysqli_prepare($koneksi, $updateTimbanganPersonQuery);
+			mysqli_stmt_bind_param($updateTimbanganPersonStmt, "si", $Status, $IDTimbangan);
+			mysqli_stmt_execute($updateTimbanganPersonStmt);
+				
+			// mysqli_query($koneksi,"update timbanganperson set StatusUTTP='$Status' where IDTimbangan='$IDTimbangan'");
+			
+			// Insert trtimbanganitem table
+			$insertTrTimbanganItemQuery = "INSERT INTO trtimbanganitem (NoTransaksi,NoUrutTrans,IDPerson,IDTimbangan,UserName,NominalRetribusi,HasilAction1,HasilAction2,TanggalTransaksi) VALUES (?,?,?,?,?,?,?,?,$TanggalNOW)";
+			$insertTrTimbanganItemStmt = mysqli_prepare($koneksi, $insertTrTimbanganItemQuery);
+			mysqli_stmt_bind_param($insertTrTimbanganItemStmt, "siiissss", $kode_jadi, $NoUrutTrans, $IDPerson, $IDTimbangan, $UserName, $NominalRetribusi, $HasilAction1, $HasilAction2);
+			$DataItem = mysqli_stmt_execute($insertTrTimbanganItemStmt);
 
-        if($DataItem){
-			echo '<script language="javascript">alert("Data Berhasil Disimpan!"); document.location="TrTeraStandart.php"; </script>';
-		}else{
-			echo '<script language="javascript">alert(Data Gagal Disimpan!"); document.location="TrTeraStandart.php"; </script>';
-		}
+            // $DataItem = @mysqli_query($koneksi, "INSERT INTO trtimbanganitem (NoTransaksi,NoUrutTrans,IDPerson,IDTimbangan,UserName,NominalRetribusi,HasilAction1,HasilAction2,TanggalTransaksi)VALUES('$kode_jadi','$NoUrutTrans','$IDPerson','$IDTimbangan','$UserName','0','$HasilAction1','$HasilAction2','$TanggalNOW')"); 
+			mysqli_stmt_close($updateTimbanganPersonStmt);
+			mysqli_stmt_close($insertTrTimbanganItemStmt);
+
+			if($DataItem){
+				echo '<script language="javascript">alert("Data Berhasil Disimpan!"); document.location="TrTeraStandart.php"; </script>';
+			}else{
+				echo '<script language="javascript">alert(Data Gagal Disimpan!"); document.location="TrTeraStandart.php"; </script>';
+			}
+        }
+        // if($DataItem){
+		// 	echo '<script language="javascript">alert("Data Berhasil Disimpan!"); document.location="TrTeraStandart.php"; </script>';
+		// }else{
+		// 	echo '<script language="javascript">alert(Data Gagal Disimpan!"); document.location="TrTeraStandart.php"; </script>';
+		// }
 
 	}else{
 		echo '<script type="text/javascript">
@@ -66,6 +117,7 @@ if(isset($_POST['SimpanData'])){
 			  });
 			  </script>';
 	}
+	mysqli_stmt_close($insertStmt);
 }		
 
 ?>
