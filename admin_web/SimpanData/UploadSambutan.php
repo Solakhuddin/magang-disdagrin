2d4453444dc0aa9118cd8f0142b971455b89977b
Solakhuddin <80704147+Solakhuddin@users.noreply.github.com>
2024-02-26 23:54:30 +0700

    Update UploadSambutan.php

--- admin_web/SimpanData/UploadSambutan.php
+++ admin_web/SimpanData/UploadSambutan.php
@@ -20,44 +20,87 @@ include ("../../library/config.php");
 			}
 
 			// filename yang aman
-			$name = preg_replace("/[^A-Z0-9._-]/i", "_", $media["name"]);
+			// $name = preg_replace("/[^A-Z0-9._-]/i", "_", $media["name"]);
+
+			$name = uniqid('file_') . '.' . $ext;
 
 			// mencegah overwrite filename
-			$i = 0;
-			$parts = pathinfo($name);
-			while (file_exists(UPLOAD_DIR . $name)) {
-				$i++;
-				$name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+			// $i = 0;
+			// $parts = pathinfo($name);
+			// while (file_exists(UPLOAD_DIR . $name)) {
+			// 	$i++;
+			// 	$name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+			// }
+
+			// Move the uploaded file to a secure directory
+			$uploadPath = '../../images/Assets/';
+			$success = move_uploaded_file($media["tmp_name"], $uploadPath . $name);
+			if (!$success) {
+				echo 'Upload file gagal.';
+				exit;
 			}
 
-			$success = move_uploaded_file($media["tmp_name"], UPLOAD_DIR . $name);
-			if ($success) {					
-				if($ext=='jpg' OR $ext=='jpeg' OR $ext=='png'){
-					//cari file untuk di hapus
-					$AmbilData = mysqli_query($koneksi,"SELECT Gambar1 FROM kontenweb WHERE JenisKonten='Sambutan'");
-					$row=mysqli_fetch_assoc($AmbilData);
-					//insert ke db.
-					$query = mysqli_query($koneksi,"UPDATE kontenweb SET Gambar1='$name' WHERE JenisKonten='Sambutan'");
-					if($query){
-						if($row['Gambar1']!=null){
-							unlink("../../images/Assets/".$row['Gambar1']."");
-							echo '<script language="javascript">alert("Data Berhasil Disimpan!");document.location="Sambutan.php";</script>';
-							// echo "Upload File Berhasil!";
-							exit;
-						}else{
-							echo "Upload File Berhasil!";
-							exit;
-						}
-					}else{
-						echo "Upload File Gagal !";
-						exit;
-					}
-				}else{
-					echo "Extension file harus .jpg/.jpeg/.png";
-					exit;
-				}
+			// Validate file extension
+			$allowedExtensions = array('jpg', 'jpeg', 'png');
+			if (!in_array($ext, $allowedExtensions)) {
+				unlink($uploadPath . $name); // Remove the uploaded file
+				echo "Extension file harus .jpg/.jpeg/.png";
+				exit;
 			}
-			chmod(UPLOAD_DIR . $name, 0644);
+
+			// Update the database
+			$stmt = mysqli_prepare($koneksi, "UPDATE kontenweb SET Gambar1=? WHERE JenisKonten='Sambutan'");
+			mysqli_stmt_bind_param($stmt, "s", $name);
+			$query = mysqli_stmt_execute($stmt);
+			mysqli_stmt_close($stmt);
+
+			if ($query) {
+				// Delete previous file
+				$previousFile = mysqli_query($koneksi, "SELECT Gambar1 FROM kontenweb WHERE JenisKonten='Sambutan'");
+				$row = mysqli_fetch_assoc($previousFile);
+				if ($row['Gambar1'] != null) {
+					unlink($uploadPath . $row['Gambar1']);
+				}
+				mysqli_free_result($previousFile);
+
+				echo '<script language="javascript">alert("Data Berhasil Disimpan!");document.location="Sambutan.php";</script>';
+				exit;
+			} else {
+				unlink($uploadPath . $name); // Remove the uploaded file
+				echo "Upload File Gagal !";
+				exit;
+			}
+
+			// $success = move_uploaded_file($media["tmp_name"], UPLOAD_DIR . $name);
+			// if ($success) {					
+			// 	if($ext=='jpg' OR $ext=='jpeg' OR $ext=='png'){
+			// 		//cari file untuk di hapus
+			// 		$AmbilData = mysqli_query($koneksi,"SELECT Gambar1 FROM kontenweb WHERE JenisKonten='Sambutan'");
+			// 		$row=mysqli_fetch_assoc($AmbilData);
+			// 		//insert ke db.
+			// 		$query = mysqli_query($koneksi,"UPDATE kontenweb SET Gambar1='$name' WHERE JenisKonten='Sambutan'");
+			// 		if($query){
+			// 			if($row['Gambar1']!=null){
+			// 				unlink("../../images/Assets/".$row['Gambar1']."");
+			// 				echo '<script language="javascript">alert("Data Berhasil Disimpan!");document.location="Sambutan.php";</script>';
+			// 				// echo "Upload File Berhasil!";
+			// 				exit;
+			// 			}else{
+			// 				echo "Upload File Berhasil!";
+			// 				exit;
+			// 			}
+			// 		}else{
+			// 			echo "Upload File Gagal !";
+			// 			exit;
+			// 		}
+			// 	}else{
+			// 		echo "Extension file harus .jpg/.jpeg/.png";
+			// 		exit;
+			// 	}
+			// }
+			// chmod(UPLOAD_DIR . $name, 0644);
+
+
 		}
 	}
 ?>
